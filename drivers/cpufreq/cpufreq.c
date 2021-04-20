b26731fff9c6413feba0aded7413d5d8fc6ec4ea
DL <dl12345@github.com>
2021-04-20 22:42:56 +0400

    drivers: cpufreq: Change the uevent notification when the CPU cluster comes back online
    Change from KOBJ_POLICY_INIT to KOBJ_ONLINE as ueventd ignores the
    POLICY_INIT event. This results in the sysfs permissions for the
    CPU frequency and governor not being set correctly and breaks the
    PowerHAL power management function.
    
    Change-Id: Idef4befb3941bbaf930cf3b1c962e4e0417483cb

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -993,6 +993,7 @@ static int cpufreq_add_dev(struct device *dev, struct subsys_interface *sif)
 	unlock_policy_rwsem_write(cpu);
 
 	kobject_uevent(&policy->kobj, KOBJ_ADD);
+    kobject_uevent(&dev->kobj, KOBJ_ONLINE);
 	module_put(cpufreq_driver->owner);
 	pr_debug("initialization complete\n");
 

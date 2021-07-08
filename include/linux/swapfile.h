7ce69f1d7cb4fe4024ec882d95bcf79cc321d7e8
SunatP <galaxysuperplus@gmail.com>
2021-07-08 17:31:07 +0700

    GPU: Change Max freq from 600 to 533 MHz, Change ZSMALLOC_NEW to ZSMALLOC
    Change-Id: I4dc66250b11557ee05760edb4f5e786c48570e9a

--- include/linux/swapfile.h
+++ include/linux/swapfile.h
@@ -8,6 +8,5 @@
 extern spinlock_t swap_lock;
 extern struct swap_list_t swap_list;
 extern struct swap_info_struct *swap_info[];
-extern int try_to_unuse(unsigned int, bool, unsigned long);
 
 #endif /* _LINUX_SWAPFILE_H */

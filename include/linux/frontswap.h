fc82b505d5f9f5ed81e5eb1dca01833d614ecc9d
SunatP <galaxysuperplus@gmail.com>
2021-07-10 21:31:25 +0700

    linux:frontswap add Struct
    Change-Id: I24305533e5bdb6ebacdd389f69d1d64a15fd4d27

--- include/linux/frontswap.h
+++ include/linux/frontswap.h
@@ -11,6 +11,7 @@ struct frontswap_ops {
 	int (*load)(unsigned, pgoff_t, struct page *);
 	void (*invalidate_page)(unsigned, pgoff_t);
 	void (*invalidate_area)(unsigned);
+	struct frontswap_ops *next; /* private pointer to next ops */
 };
 
 extern bool frontswap_enabled;
